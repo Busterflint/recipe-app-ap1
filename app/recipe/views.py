70e970b1b5e57ddd9a870312db749a33460622d9
Busterflint <busterflint@outlook.com>
2020-08-06 21:03:57 +0200

    Added create tag feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,7 +13,9 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 """ Create your viewset """
-class TagViewSet(viewsets.GenericViewSet,mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     """Add the authentication classes"""
     authentication_classes = (TokenAuthentication,)
@@ -26,3 +28,7 @@ class TagViewSet(viewsets.GenericViewSet,mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag."""
+        serializer.save(user=self.request.user)
