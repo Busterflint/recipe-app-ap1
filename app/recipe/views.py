6d073c244ef5abde49de313019974293c5c5ee38
Busterflint <busterflint@outlook.com>
2020-08-11 12:19:39 +0200

    Added recipe image upload feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 """ Create a viewset - generic viewset - use the list model mixit """
 """ Use a combination of the two."""
 """ It is a django rest model feature. You can pull in different parts of a viewset."""
 """ We only want the list model function. We do not want the create,update,delete function"""
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 """ Add the token authentication. You want to authenticate the request. """
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -58,9 +60,31 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """ Return appropriate serializer class """
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
         return self.serializer_class
 
     def perform_create(self, serializer):
         """ Create a new recipe """
         serializer.save(user=self.request.user)
-        
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data = request.data
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status = status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+        )
